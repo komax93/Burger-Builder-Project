38124d450676b81eb5788c82f4989c473d01235e
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2018-01-06 22:49:52 +0200

    added animation to modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,7 +24,8 @@ class BurgerBuilder extends Component{
         meat: 0
       },
       totalPrice: 0,
-      purchasable: false
+      purchasable: false,
+      purchasing: false
     };
   }
 
@@ -73,6 +74,10 @@ class BurgerBuilder extends Component{
     this.updatePurchaseState(updatedIngredients);
   };
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -84,7 +89,7 @@ class BurgerBuilder extends Component{
 
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients}/>
         </Modal>
         <Burger ingredients={this.state.ingredients}/>
@@ -93,6 +98,7 @@ class BurgerBuilder extends Component{
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchasable={this.state.purchasable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice}
         />
       </Aux>
